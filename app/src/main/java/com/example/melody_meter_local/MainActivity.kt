66da64708f5c2d9b49a1b29678ed1c650c9ef5cd
magg-ting <34826140+magg-ting@users.noreply.github.com>
2024-07-19 10:50:35 -0400

    portrait alignments, except home where bottom nav bar blocks the view

--- app/src/main/java/com/example/melody_meter_local/MainActivity.kt
+++ app/src/main/java/com/example/melody_meter_local/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.melody_meter_local
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.melody_meter_local.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,18 +24,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //FirebaseApp.initializeApp(this)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navBar
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
 
-        navHostFragment.let {
-            navController = it.navController
-            navView.setupWithNavController(navController)
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            binding.bottomNavBar?.setupWithNavController(navController)
         }
+        else {
+            binding.btnHome?.setOnClickListener {
+                navController.navigate(R.id.navigation_home)
+            }
+            binding.btnSearch?.setOnClickListener {
+                navController.navigate(R.id.navigation_search)
+            }
+            binding.btnProfile?.setOnClickListener {
+                navController.navigate(R.id.navigation_profile)
+            }
+        }
+
     }
 }
